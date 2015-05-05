54314a988d142564d89779755d28465e8ddc7450
unknown <Guillermo.nunez@JMIN-D1422.formation.enjmin.fr>
2015-05-05 17:06:11 +0200

    Adaptation des scripts pour VS2013
    Asset Pathfinding eliminé

---
+++ Assets/AI/Actions/clearPath.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using RAIN.Action;
+using RAIN.Core;
+
+[RAINAction]
+public class clearPath : RAINAction
+{
+    public override void Start(RAIN.Core.AI ai)
+    {
+        base.Start(ai);
+    }
+
+    public override ActionResult Execute(RAIN.Core.AI ai)
+    {
+		Debug.Log("CLEARNEDE" );
+
+
+		if (ai.Navigator.CurrentPath != null) {
+			Debug.Log ("CLEARNEDEEEEEEEEEEEEEEEEEEEEEEEEEEESSSSSSSSSSSSSSSSSSS" + ai.Navigator.CurrentPath.IsValid);
+			Debug.Log ("CLEARNEDEEEEEEE22222" + ai.Navigator.CurrentPath.IsPartial);
+
+			//ai.Reset (ai.Navigator.CurrentPath.);
+
+			ai.Navigator.CurrentPath = null;
+
+
+		}
+		//tNav.RestartPathfindingSearch();
+
+        return ActionResult.FAILURE;
+    }
+
+    public override void Stop(RAIN.Core.AI ai)
+    {
+        base.Stop(ai);
+    }
+}
