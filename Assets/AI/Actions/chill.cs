e357bf69527a6b4fe6b803d1a93d011765bf8beb
GNunez <GNU@gui.com>
2015-05-14 16:44:15 +0200

    commentaires sur work et chill

--- Assets/AI/Actions/chill.cs
+++ Assets/AI/Actions/chill.cs
@@ -8,65 +8,36 @@ using RAIN.Representation;
 [RAINAction]
 public class chill : RAINAction
 {
-
-  //  public Expression myExpression;
-  // public  Vector2 testVect;
-
-
     public override void Start(RAIN.Core.AI ai)
     {
-       // testVect = myExpression.Evaluate(ai.DeltaTime, ai.WorkingMemory).GetValue<Vector2>();
-
-      //  Debug.Log("CHIL START  ");
-
+		//Set de bool, sert a rien pour l'instant
         ai.WorkingMemory.SetItem("enDeplacement", false);
         base.Start(ai);
     }
 
     public override ActionResult Execute(RAIN.Core.AI ai)
     {
-        // setTaget(null);
-
-      //  Debug.Log("PUTAIN DE CHILL  ");
-
-
-        
-       //  while (ai.WorkingMemory.GetItem<int>("motivation") < ai.WorkingMemory.GetItem<int>("motivationMax"))
-      //  {
+			//Reduction de la fatigue si existante
             if (ai.WorkingMemory.GetItem<int>("fatigue") > 0)
                 ai.WorkingMemory.SetItem("fatigue", ai.WorkingMemory.GetItem<int>("fatigue") - (int)ai.Body.gameObject.GetComponent<Employe>().vitesseFatigue);
           
-        ai.WorkingMemory.SetItem("motivation", ai.WorkingMemory.GetItem<int>("motivation") + ai.Body.gameObject.GetComponent<Employe>().effetRepos);
-            
-           // yield return new WaitForSeconds(1.0f / vitesseTravail);
-     //   }
-       // print("CHANGE TARGET: " + boxDeTravail);
-       // ai.WorkingMemory.SetItem("enDeplacement", true);
-
-         //   Debug.Log("PUTAIN DE MLIDDLE DE CHILLLLLL  ");
-
-
+			//Augmente la motivation
+        	ai.WorkingMemory.SetItem("motivation", ai.WorkingMemory.GetItem<int>("motivation") + ai.Body.gameObject.GetComponent<Employe>().effetRepos);
+           
+			//Si motivation Max, cesse de glander
             if (ai.WorkingMemory.GetItem<int>("motivation") >= ai.WorkingMemory.GetItem<int>("motivationMax"))
-            {
-
-                 ai.WorkingMemory.GetItem<GameObject>("myTarget").GetComponent<Repos>().occupe = false;
-
+            {	
+				//Libere la place (encore en test)
+                ai.WorkingMemory.GetItem<GameObject>("myTarget").GetComponent<Repos>().occupe = false;
                  return ActionResult.SUCCESS;
             }
 
-
-        //    Debug.Log("PUTAIN DE FAILLLLLLLLL  ");
-
-
-
+			//Continue a glander
             return ActionResult.RUNNING;
     }
 
     public override void Stop(RAIN.Core.AI ai)
     {
-
-
-
         base.Stop(ai);
     }
 }
--- Assets/AI/Actions/work.cs
+++ Assets/AI/Actions/work.cs
@@ -9,62 +9,33 @@ public class work : RAINAction
 {
     public override void Start(RAIN.Core.AI ai)
     {
-
-
-       // Debug.Log("PUTAIN DE WORKKKK  STARTTT ");
-
         base.Start(ai);
         
-        //Chaque seconde : motivation -= feignantise DONC si feignantise est grand, les pauses seront plus fréquentes.
-        ai.WorkingMemory.SetItem("working", true);
-        // auTravail = true;
-        //setTaget(null);	
-        //setTaget(chill[index]);
+		//Set de bools, sert a rien pour l'instant
+		ai.WorkingMemory.SetItem("working", true);
         ai.WorkingMemory.SetItem("enDeplacement", false);
     }
 
     public override ActionResult Execute(RAIN.Core.AI ai)
     {
-       // Debug.Log("PUTAIN DE WORKKKK  ");
-
-    //    while (ai.WorkingMemory.GetItem<int>("motivation") > 0)
-     //   {
-            //print("MOTIV: "+ motivation);
-            //fatigue += vitesseFatigue;
-
-
+       //Reduction de la motivation
         ai.WorkingMemory.SetItem("motivation", ai.WorkingMemory.GetItem<int>("motivation") - ai.Body.GetComponent<Employe>().feignantise);
 
-    //    ai.Body.GetComponent<Employe>().motivation -= ai.Body.GetComponent<Employe>().feignantise;
-           // ai.WorkingMemory.SetItem("motivation", mot);
-           // yield return new WaitForSeconds((float)(1.0f / vitesseTravail));
-      //  }
-
-     //   int index = Random.Range(0, chill.Length);
-       // print("CHANGE TARGET: " + chill[index]);
-      
-
-       // ai.WorkingMemory.SetItem("enDeplacement", true);
-
-        //setTaget(chill[index]);
-       // ai.WorkingMemory.SetItem("chillTarget", chill[index].transform.position);
-
-
+   		//Finis de travailler quand la motivation est 0
         if (ai.WorkingMemory.GetItem<int>("motivation") <= 0)
         {
+			//Libere la place (encore en test)
             ai.WorkingMemory.GetItem<GameObject>("myTarget").GetComponent<Box>().occupe = false;
             return ActionResult.SUCCESS;
-
         }
 
-                   // Debug.Log("PUTAIN DE WORKKKK RUNNNNNNNNNNNNNNNNNNNNNN ");
-
-
+		//Continue a travailler
          return ActionResult.RUNNING;
     }
 
     public override void Stop(RAIN.Core.AI ai)
     {
+		//Set de bool, sert a rien pour l'instant
         ai.WorkingMemory.SetItem("working", false);
         base.Stop(ai);
     }
