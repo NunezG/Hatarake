5fdc8992de54afe85c8b7a40ca8c06116f94286a
unknown <Guillermo.nunez@JMIN-D1422.formation.enjmin.fr>
2015-05-19 14:06:24 +0200

    Barre de progrès

---
+++ Assets/Script/ProgressBar.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProgressBar : MonoBehaviour {
+
+
+    public Texture progressBackground;
+    public Texture progressForground;
+
+     
+
+    void OnGUI()
+    {
+
+       // print("ONGUI ");
+        DrawProgress(GetComponent<GameManager>().objectiveCompletion / 1000.0f);
+       
+    }
+
+
+
+ 
+void DrawProgress(float progress )
+{
+    GUI.DrawTexture(new Rect(10, 10, 200, 30), progressBackground);
+    GUI.DrawTexture(new Rect(10, 10, 200 * progress, 30), progressForground);
+}
+}
