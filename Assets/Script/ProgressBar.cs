fd6ac2d9515ae1ce65e4764463913235197f05ce
Plemar <aldebaranaintbadtoo@gmail.com>
2015-06-03 11:56:20 +0200

    Boucle complétion objectif

--- Assets/Script/ProgressBar.cs
+++ Assets/Script/ProgressBar.cs
@@ -6,12 +6,27 @@ public class ProgressBar : MonoBehaviour {
     public Texture progressForeground;
     public Texture waitScreen;
     public Texture ki;
+    public RectTransform valueKi;
+    float unitaryX, unitaryWidth;
+    public GameObject boss = null;
 
-    public GameObject boss=null;
+    void Start()
+    {
+        unitaryX = valueKi.anchoredPosition.x;
+        unitaryWidth = valueKi.rect.width;
+
+    }
 
     void Update()
     {
         if (boss == null) boss = GameObject.FindGameObjectWithTag("Boss");
+        else
+        {
+
+            valueKi.sizeDelta = new Vector2(unitaryWidth * ((float)boss.GetComponent<Boss>().yellingO_Meter/2), valueKi.rect.height);
+            valueKi.anchoredPosition = new Vector2(valueKi.sizeDelta.x/2, valueKi.anchoredPosition.y);
+        }
+
     }
 
 
@@ -23,12 +38,13 @@ public class ProgressBar : MonoBehaviour {
 		}
         if (boss != null)
         {
-            DrawYellingOMeter((float)(boss.GetComponent<Boss>().yellingO_Meter) / (float)(boss.GetComponent<Boss>().maxYellingO_Meter));
+            //DrawYellingOMeter((float)(boss.GetComponent<Boss>().yellingO_Meter) / (float)(boss.GetComponent<Boss>().maxYellingO_Meter));
         }
         DrawProgressObjective(GameManager.instance.objectiveCompletion / GameManager.instance.levelObjective);
         if (GameManager.instance.GetComponent<CharacterManager>().GetTotalNumberOfBoxies() != 0)
             DrawNumberOfWorkingEmploye(GameManager.instance.GetComponent<CharacterManager>().GetNumberOfWorkingBoxies(), GameManager.instance.GetComponent<CharacterManager>().GetTotalNumberOfBoxies());
        
+    
     }
 
 	
