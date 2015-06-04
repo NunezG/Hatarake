27db77d89258cb4ef5e9a51ef284cc15eb921848
Bricedre <bricedre@hotmail.com>
2015-06-04 11:45:54 +0200

    Nouvelle police de caractère pour les mots japonais :) Music Manager qui balance la musique au début

--- Assets/Script/MusicManager.cs
+++ Assets/Script/MusicManager.cs
@@ -3,7 +3,6 @@ using System.Collections;
 
 public class MusicManager : MonoBehaviour {
 
-	public AudioSource ambiance;
 	public AudioSource mainBG;
 	public AudioSource[] tensionsLayers;
 	public float[] tensionsLayersVolumes;
@@ -12,12 +11,10 @@ public class MusicManager : MonoBehaviour {
 	void Start () {
 	
 		// triggering ambiance sound
-		ambiance = transform.FindChild ("ambianceSound").GetComponent<AudioSource> ();
 		mainBG = transform.FindChild ("mainBackground").GetComponent<AudioSource> ();
 
 		// triggering BG music
-		//ambiance.Play ();
-		//mainBG.Play ();
+		mainBG.Play ();
 
 		// triggering tension layers so they are in synch
 		for (int i = 0 ; i < tensionsLayers.Length ; i++)
