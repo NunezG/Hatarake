ddc8b8c831d3bcc42e270d6831adeeb33ce9290a
Bricedre <bricedre@hotmail.com>
2015-06-08 10:12:45 +0200

    -Modifications de certains assets -Idle de la tete des décrasseurs -Script de Random dans des ensembles discontinus

---
+++ Assets/ArithmeticShit.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArithmeticShit : MonoBehaviour {
+ 
+	float SpreadOutRandom(Vector2[] ranges){
+
+		//Initialize Spread for Random max value
+		float spread = 0.0f;
+
+		//Add up all the ranges
+		for (int i = 0; i < ranges.Length; i++) {
+			spread += (ranges [i].y - ranges [i].x);
+		}
+
+		//Initialize the randomly chosen value
+		float value = Random.Range(0, spread);
+
+		//Go through all the ranges
+		for(int i = 0; i < ranges.Length ; i++){ 
+
+			//If you can subtract the current range from the value, do it!
+			if((value - (ranges[i].y - ranges[i].x)) > 0.0f ){
+				value -= (ranges[i].y - ranges[i].x);
+			}
+
+			//If the value is inferior to the current range, add it to the left limit!
+			else{ 
+				value += ranges[i].x;
+				break;
+			}
+		}
+
+		return value;
+
+	}
+}
